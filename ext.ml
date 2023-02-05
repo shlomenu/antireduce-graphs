888efb3564deb881569b79bc30ea7e42a296813c
Eli Whitehouse <eliw55@gmail.com>
2023-02-05 18:50:38 -0500

    break out helper functions useful for debugging

--- ext.ml
+++ ext.ml
@@ -16,20 +16,23 @@ let parse_program_exn ?(primitives = Eval.all_primitives) =
 let parse_stitch_invention_exn ?(primitives = Eval.all_primitives) =
   Parser.parse_stitch_invention_exn primitives
 
+let apply_to_state (f : Program.t) : Program.t =
+  Apply
+    ( Primitive {name= "save"; ty= Eval.graph_transform}
+    , Apply (f, Primitive {name= "initial"; ty= Eval.graph_state}) )
+
+let retrieve_result () = Util.value_exn !State.Operations.last_found
+
+let nontrivial g = Graph.size g > 1 && Graph.n_components g = 1
+
 let explore ~exploration_timeout ~max_novel_representations ~program_size_limit
     ~eval_timeout ~attempts ~dsl ~representations_dir j =
-  let max_conn = SU.to_int @@ SU.member "max_conn" j in
-  let retrieve_result () = Util.value_exn !State.Operations.last_found in
   Exploration.multikey_explore ~exploration_timeout ~max_novel_representations
     ~program_size_limit ~eval_timeout ~attempts ~dsl ~representations_dir
-    ~apply_to_state:(fun f ->
-      Apply
-        ( Primitive {name= "save"; ty= Eval.graph_transform}
-        , Apply (f, Primitive {name= "initial"; ty= Eval.graph_state}) ) )
-    ~evaluate:(Eval.evaluate ~max_conn) ~retrieve_result
-    ~nontrivial:(fun g -> Graph.size g > 1 && Graph.n_components g = 1)
-    ~parse:parse_program_exn ~request:Eval.graph_transform
-    ~yojson_of_output:Graph.yojson_of_t
+    ~apply_to_state
+    ~evaluate:(Eval.evaluate ~max_conn:(SU.to_int @@ SU.member "max_conn" j))
+    ~retrieve_result ~nontrivial ~parse:parse_program_exn
+    ~request:Eval.graph_transform ~yojson_of_output:Graph.yojson_of_t
     ~keys_of_output:(fun g ->
       let view = Graph.Frozen.of_graph g in
       (Graph.Key.of_frozen view, view) )
